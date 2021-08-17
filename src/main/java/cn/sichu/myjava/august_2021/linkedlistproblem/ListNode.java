b96c7149191beffd055a84b49afc10759d410a46
sichu huang <sichu.huang2021@outlook.com>
2021-08-17 03:42:42 +0100

    Aug 17

---
+++ src/main/java/cn/sichu/myjava/august_2021/linkedlistproblem/ListNode.java
@@ -0,0 +1,18 @@
+package cn.sichu.myjava.august_2021.linkedlistproblem;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	public ListNode(int val, ListNode next) {
+		super();
+		this.val = val;
+		this.next = next;
+	}
+
+	public ListNode(int val) {
+		super();
+		this.val = val;
+	}
+
+}

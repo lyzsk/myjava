22ad029153caa32fbd7af5354e72a37e1af1996e
sichu huang <sichu.huang2021@outlook.com>
2021-08-20 16:07:18 +0100

    Aug 20 16:07

--- src/main/java/cn/sichu/myjava/august2021/arrayproblem/FourSum.java
+++ src/main/java/cn/sichu/myjava/august2021/arrayproblem/FourSum.java
@@ -1,5 +1,8 @@
 package cn.sichu.myjava.august2021.arrayproblem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -20,6 +23,44 @@ import java.util.List;
  */
 public class FourSum {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        // TODO complete
+        int n = nums.length;
+        if (n < 4) {
+            return Collections.emptyList();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < n - 3; i++) {
+            // a、b、c 和 d 互不相同
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        ++k;
+                        --l;
+                        while (k < n && nums[k] == nums[k - 1]) {
+                            ++k;
+                        }
+                        while (i > j && nums[l] == nums[l + 1]) {
+                            --l;
+                        }
+                    } else if (nums[i] + nums[j] + nums[k] + nums[l] < target) {
+                        ++k;
+                    } else {
+                        --l;
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 }
--- src/main/java/cn/sichu/myjava/august2021/arrayproblem/ThreeSumClosest.java
+++ src/main/java/cn/sichu/myjava/august2021/arrayproblem/ThreeSumClosest.java
@@ -1,5 +1,7 @@
 package cn.sichu.myjava.august2021.arrayproblem;
 
+import java.util.Arrays;
+
 /**
  * 16. 最接近的三数之和 @see<a href = "https://leetcode-cn.com/problems/3sum-closest">最接近的三数之和</a>
  * <p>
@@ -16,6 +18,39 @@ package cn.sichu.myjava.august2021.arrayproblem;
  */
 public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = 0;
+        int n = nums.length;
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 2; i++) {
+            int tmp = twoSumClosest(nums, i + 1, n - 1, target - nums[i]);
+            if (Math.abs(nums[i] + tmp - target) < diff) {
+                res = nums[i] + tmp;
+                diff = Math.abs(nums[i] + tmp - target);
+            }
+        }
+        return res;
+    }
 
+    private int twoSumClosest(int[] nums, int start, int end, int target) {
+        int res = 0;
+        int diff = Integer.MAX_VALUE;
+
+        while (start < end) {
+            int val = nums[start] + nums[end];
+            if (val == target) {
+                return val;
+            }
+            if (Math.abs(val - target) < diff) {
+                res = val;
+                diff = Math.abs(val - target);
+            }
+            if (val < target) {
+                ++start;
+            } else {
+                --end;
+            }
+        }
+        return res;
     }
 }

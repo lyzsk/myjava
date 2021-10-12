e0a734d827fe31f4b4fc733edc2e249b3dcf29ce
sichu <sichu.huang2021@outlook.com>
2021-10-12 18:45:10 +0100

    Oct 12, 18:45

--- src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/LongestPalindrome.java
+++ src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/LongestPalindrome.java
@@ -25,6 +25,70 @@ package cn.sichu.myjava.october2021.dynamicprogramming;
  */
 public class LongestPalindrome {
     public String longestPalindrome(String s) {
+        int len = s.length();
+        // dp[i][j] 表示 s[i..j] 是否为回文串
+        boolean[][] dp = new boolean[len][len];
+        int start = 0;
+        int maxlen = 1;
 
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (j - i < 2) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+                }
+                // 只要 dp[i][j] == true 成立， 就表示 子串为回文， 记录 起始位置 和 回文长度
+                if (dp[i][j] && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start + maxlen);
     }
+
+    // TODO: 下面这个快，但是没懂
+
+    // public String longestPalindrome(String s) {
+    // if (s == null || s.length() == 0) {
+    // return "";
+    // }
+    //
+    // // 保存起始位置， 用数组比全局变量快
+    // int[] range = new int[2];
+    // char[] str = s.toCharArray();
+    //
+    // for (int i = 0; i < s.length(); i++) {
+    // // 把回文看成中间部分全是同一字符，左右部分对称
+    // // 找到下一个与当前字符不同的字符
+    // i = findLogest(str, i, range);
+    // }
+    // return s.substring(range[0], range[1] + 1);
+    // }
+    //
+    // public static int findLogest(char[] str, int low, int[] range) {
+    // // 查找中间部分
+    // int high = low;
+    // while (high < str.length - 1 && str[high + 1] == str[low]) {
+    // high++;
+    // }
+    //
+    // // 定位中间部分最后一个字符
+    // int ans = high;
+    //
+    // // 从中间向左右扩散
+    // while (low > 0 && high < str.length - 1 && str[low - 1] == str[high + 1]) {
+    // low--;
+    // high++;
+    // }
+    //
+    // // 记录最大长度
+    // if (high - low > range[1] - range[0]) {
+    // range[0] = low;
+    // range[1] = high;
+    // }
+    //
+    // return ans;
+    // }
 }

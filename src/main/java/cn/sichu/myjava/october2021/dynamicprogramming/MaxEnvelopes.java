a6bddfd0e1525ec4c4bf0c6d21b736d2efc7abd9
sichu <sichu.huang2021@outlook.com>
2021-10-13 16:01:40 +0100

    16:01 Oct 13

--- src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/MaxEnvelopes.java
+++ src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/MaxEnvelopes.java
@@ -39,6 +39,18 @@ public class MaxEnvelopes {
         Arrays.sort(envelopes, (a, b) -> {
             return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
         });
-        return 0;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
     }
 }
--- src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/MaxValue.java
+++ src/main/java/cn/sichu/myjava/october2021/dynamicprogramming/MaxValue.java
@@ -22,7 +22,42 @@ package cn.sichu.myjava.october2021.dynamicprogramming;
  * @date 2021/10/03
  */
 public class MaxValue {
-    public int maxValue(int[][] grid) {
 
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 不好理解
+     * 
+     * @param grid
+     * @return
+     */
+    public int maxValueTwo(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1] + grid[i - 1][j - 1]);
+            }
+        }
+        return dp[m][n];
     }
 }

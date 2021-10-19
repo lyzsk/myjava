ac8b2888cf6518f395d713260b738b03936a3d18
sichu <sichu.huang2021@outlook.com>
2021-10-19 16:55:34 +0100

    Oct 19 16:55更改为printf打印

--- src/main/java/cn/sichu/myjava/october2021/cuproblem/Rsacrypt.java
+++ src/main/java/cn/sichu/myjava/october2021/cuproblem/Rsacrypt.java
@@ -9,13 +9,13 @@ public class Rsacrypt {
 
         // alice PublicKey 加密
         byte[] publick = alice.getPublicKey();
-        System.out.println(String.format("public key: %x", new BigInteger(1, publick)));
+        System.out.printf("public key: %x\n", new BigInteger(1, publick));
 
         byte[] encrypted = alice.encrypt(plaintext);
         System.out.printf("encrypted: %x\n", new BigInteger(1, encrypted));
 
         byte[] privatek = alice.getPrivateKey();
-        System.out.println(String.format("private key: %x", new BigInteger(1, privatek)));
+        System.out.printf("private key: %x\n", new BigInteger(1, privatek));
         byte[] decrypted = alice.decrypt(encrypted);
         System.out.println(new String(decrypted, "UTF-8"));
 
